1b484ed870e2d2d5e221d07449d1da6a0fc1b291
Danvers <signorini@gmail.com>
2022-10-14 20:38:55 -0300

    Auto-generated commit [ci skip]

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+alert ('Seja bem-vindo ao nosso novo site!')
+
+function abreFechaMenu() {
+    // navbar -tag nav
+    let navbar = document.querySelector(".submenuj");
+    navbar.classList.toggle("mostraEscondeMenu");
+    // console.log(navbar);
+}
+
+
