34d3ff1bdece1ae24c1081c4c26c8475a25d8806
Danvers <signorini@gmail.com>
2022-10-14 20:20:23 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { ProdutosComponent } from './views/produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: ProdutosComponent},
+  {path: "login", component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProdutosComponent } from './views/produtos/produtos.component';
+import { LoginComponent } from './views/login/login.component';
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ProdutosComponent,
+    LoginComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
